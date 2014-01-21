5cbb2b2422a26fc0d1ebf2a4b46f9c839a98f0ba
Ewa Marciniak <emarciniak78@gmail.com>
2014-01-21 01:43:51 +0000

    Ensuring that the process exits if the checks fail

--- myscript.sh
+++ myscript.sh
@@ -6,17 +6,26 @@ clear
 START=$(date +%s.%N)
 echo Starting deployment process
 echo .....
-echo Providing a clean environment
-echo .....
+
 #check if memory used is less that 90%
 source /home/testuser/project/check_memory.sh
-if [ $(echo "$USEDPCT < 90" | bc) -ne 0 ]; then
+if [ $(echo "$USEDPCT < 90" | bc) -eq 0 ]; then
+    echo Cannot continue, the memory used is at the level of 90% or above
+    perl /home/testuser/project/mailer.pl
+    exit
+fi
 
 #check if memory used is less that 90%
 source /home/testuser/project/check_disc_space.sh
-if [ $ERRORCOUNT -eq 0 ]; then
+if [ $ERRORCOUNT -ne 0 ]; then
+    echo Cannot continue, disc space used is at a dangerously high level
+    perl /home/testuser/project/mailer.pl
+    exit
+fi
 
 #start clean environment using external script - clean.sh
+echo Providing a clean environment
+echo .....
 source /home/testuser/project/clean.sh
 
 cd /tmp
@@ -36,19 +45,22 @@ mkdir deploy
 #check if apache and mysql are running
 source /home/testuser/project/check_services_running.sh
 if [ $ERRORCOUNT -eq 0 ]; then
-echo Both mysql and apache2 services are running
-echo .....
+    echo Both mysql and apache2 services are running
+    echo .....
 else
-echo The services are not running, Error count is higher than 0
-perl /home/testuser/project/mailer.pl
-exit 1
+    echo The services are not running, Error count is higher than 0
+    perl /home/testuser/project/mailer.pl
+    exit
 fi
-
 
 #run second script will to check if the ports are listening and 
 #proceed with build, integrate and test if error count equal to 0
 source /home/testuser/project/check_ports_listening.sh
-if [ $ERRORCOUNT -eq 0 ]; then
+if [ $ERRORCOUNT -ne 0 ]; then
+    echo Cannot continue, the ports are not listening, Error count is higher than 0
+    perl /home/testuser/project/mail
+    exit
+fi
 
 #BUILD
 #runs build process from external script - build.sh
@@ -66,22 +78,7 @@ source /home/testuser/project/test.sh
 #runs deploy process from external script - deploy.sh
 source /home/testuser/project/deploy.sh
 
-
 service cron start
-else
-echo Cannot continue, the ports are not listening, Error count is higher than 0
-perl /home/testuser/project/mailer.pl
-fi
-
-else 
-echo Cannot continue, disc space used is at a dangerously high level
-perl /home/testuser/project/mailer.pl
-fi
-
-else 
-echo Cannot continue, the memory used is at the level of 90% or above
-perl /home/testuser/project/mailer.pl
-fi
 echo .....
 END=$(date +%s.%N)
 DIFF=$(echo "($END - $START)" | bc)
